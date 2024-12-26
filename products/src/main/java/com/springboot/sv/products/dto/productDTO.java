dea4a62b01c1b695fb29ba0a9b8579a6b6a77527
Marco Gerardo Serrano Lopez <marcolopez121@outlook.com>
2024-12-26 15:44:45 -0600

    Initial proyect products

---
+++ products/src/main/java/com/springboot/sv/products/dto/productDTO.java
@@ -0,0 +1,10 @@
+package com.springboot.sv.products.dto;
+
+public class productDTO {
+    private Long id;
+    private String title;
+    private Double price;
+    private String description;
+    private String category;
+    private String image;
+}
