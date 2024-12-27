4d3129eff5e5b976fcc04c926166fbbba5d4cd88
Marco Gerardo Serrano Lopez <marcolopez121@outlook.com>
2024-12-26 19:47:04 -0600

    Implementing Logs in the Service

--- products/products/src/main/java/com/springboot/sv/products/service/ProductServiceImpl.java
+++ products/products/src/main/java/com/springboot/sv/products/service/ProductServiceImpl.java
@@ -3,6 +3,8 @@ package com.springboot.sv.products.service;
 import com.springboot.sv.products.ProductsApplication;
 import com.springboot.sv.products.dto.GenericResponseDto;
 import com.springboot.sv.products.dto.ProductDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +15,7 @@ import java.util.List;
 @Service
 public class ProductServiceImpl implements ProductService{
 
+    private static final Logger logger = LoggerFactory.getLogger(ProductService.class);
     private static final String EXTERNAL_API_URL = "https://fakestoreapi.com/products";
 
     @Autowired
@@ -25,12 +28,14 @@ public class ProductServiceImpl implements ProductService{
             ProductDto[] products = restTemplate.getForObject(EXTERNAL_API_URL, ProductDto[].class);
             List<ProductDto> productDtoList = Arrays.asList(products);
 
+            logger.info("Products successfully recovered: {}", productDtoList);
             return GenericResponseDto.<List<ProductDto>>builder()
                     .data(productDtoList)
                     .message("Successfull operation.")
                     .status(200)
                     .build();
         }catch (Exception e){
+            logger.warn("Products  not found");
             return GenericResponseDto.<List<ProductDto>>builder()
                     .data(null)
                     .message("Error in operation.")
@@ -46,12 +51,14 @@ public class ProductServiceImpl implements ProductService{
         ProductDto productDto = restTemplate.getForObject(newUrl,ProductDto.class);
 
         if(productDto == null){
+            logger.warn("Product with ID {} not found", id);
             return  GenericResponseDto.<ProductDto>builder()
                     .data(null)
                     .message("Product not found for ID: " + id)
                     .status(404)
                     .build();
         }
+        logger.info("Product successfully recovered: {}", productDto.getTitle());
         return  GenericResponseDto.<ProductDto>builder()
                 .data(productDto)
                 .message("Successfull operation.")
