fca2087161f15047cca49495cf8f1b9f875df327
Marco Gerardo Serrano Lopez <marcolopez121@outlook.com>
2024-12-29 15:17:20 -0600

    Implementing Logs in the OrderController

--- backend/backend/src/main/java/com/springboot/sv/backend/controller/OrderController.java
+++ backend/backend/src/main/java/com/springboot/sv/backend/controller/OrderController.java
@@ -5,6 +5,8 @@ import com.springboot.sv.backend.entities.Order;
 import com.springboot.sv.backend.entities.Payment;
 import com.springboot.sv.backend.service.OrderService;
 import com.springboot.sv.backend.service.PaymentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrderController {
+
+    private  static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
     @Autowired
     private OrderService orderService;
@@ -21,13 +25,17 @@ public class OrderController {
 
     @PostMapping
     public ResponseEntity<?> createOrder(@RequestBody OrderDto order) {
+        logger.info("Accessing the method createOrder");
         GenericResponseDto<OrderResponseDto> response = orderService.createOrder(order);
+        logger.info("Information {}", response.getData());
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @PostMapping("/{id}/pay")
     public ResponseEntity<?> payOrder(@PathVariable Long id){
+        logger.info("Accessing the method processPayment");
         GenericResponseDto<OrderResponseDto> response = paymentService.processPayment(id);
+        logger.info("Information {}", response.getData());
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 }
