9171b2f134fbd16999a0e62d7f441b45472bab16
Marco Gerardo Serrano Lopez <marcolopez121@outlook.com>
2024-12-30 19:39:41 -0600

    Change in ProductDto

--- products/products/src/main/java/com/springboot/sv/products/dto/ProductDto.java
+++ products/products/src/main/java/com/springboot/sv/products/dto/ProductDto.java
@@ -7,7 +7,7 @@ public class ProductDto {
     private Long id;
     private String title;
     private Double price;
+    //private String image;
     private String description;
-    private String category;
-    private String image;
+    //private String category;
 }
